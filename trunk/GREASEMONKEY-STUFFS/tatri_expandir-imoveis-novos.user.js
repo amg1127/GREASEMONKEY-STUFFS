9794cd83f183b1b94ab9afbeb38a8394ff9c0c4f
amg1127 <amg1127@5465fe1f-e9f4-4a51-a9b0-42098c041c11>
2011-06-26 00:36:53 +0000

    Tentei arrumar o script, para que ele volte a expandir automaticamente os imoveis com garagem e que nao foram visitados na imobiliaria original. Mas nao poderei completar o conserto por causa disso: http://hacks.mozilla.org/2010/03/privacy-related-changes-coming-to-css-vistited/

--- trunk/GREASEMONKEY-STUFFS/tatri_expandir-imoveis-novos.user.js
+++ trunk/GREASEMONKEY-STUFFS/tatri_expandir-imoveis-novos.user.js
@@ -23,7 +23,7 @@ function tem_garagem (descricao) {
 }
 
 function onload_func () {
-    var i, obj, colec, descrdivs, manda, ohtm, obr, odiv, descricao, linha, c, olink, disparar, s;
+    var i, obj, colec, descrdivs, manda, ohtm, obr, odiv, descricao, linha, c, olink, disparar, s, acor;
     var evt = document.createEvent("MouseEvents");
     evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
     colec = document.images;
@@ -33,39 +33,18 @@ function onload_func () {
         obj = colec[i];
         manda = false;
         if (obj.className == "imgShowDetails") {
-            if (obj.src.indexOf("icon_plus_v2.png") >= 0) {
+            if (obj.alt == "Exibir detalhes") {
                 if (window.getComputedStyle(obj, null).getPropertyValue("display") == "inline") {
                     manda = true;
                     linha = obj.parentNode.parentNode;
                     descrdivs = linha.getElementsByClassName("divDescription");
                     if (descrdivs.length == 1) {
-                        ohtm = descrdivs[0].innerHTML;
-                        obr = ohtm.indexOf("<br>");
-                        odiv = ohtm.indexOf("<div class=\"divCompanyName\">");
-                        if (obr >= 0 && obr < odiv) {
-                            descricao = Trim (ohtm.substring(obr + 4, odiv));
-                            if (descricao && descricao.indexOf("<") < 0 && descricao.indexOf(">") < 0) {
-                                descricao = descricao.toLowerCase();
-                                if (! tem_garagem (descricao)) {
-                                    descrdivs = linha.getElementsByClassName("tdAddress");
-                                    if (descrdivs.length == 1) {
-                                        ohtm = descrdivs[0].innerHTML;
-                                        odiv = ohtm.indexOf("<div class=\"divDetails\">");
-                                        if (odiv >= 0) {
-                                            descricao = Trim (ohtm.substring (0, odiv));
-                                            odiv = ohtm.indexOf("<img alt=\"Icon_new\" src=\"/images/icon_new.png?");
-                                            if (odiv >= 0) {
-                                                descricao = Trim (ohtm.substring (0, odiv));
-                                            }
-                                            if (descricao && descricao.indexOf("<") < 0 && descricao.indexOf(">") < 0) {
-                                                descricao = descricao.toLowerCase();
-                                                if (! tem_garagem (descricao)) {
-                                                    manda = false;
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+                        ohtm = descrdivs[0];
+                        if (ohtm.childNodes.length > 2) {
+                            descricao = Trim (ohtm.childNodes[2].nodeValue);
+                            descricao = descricao.toLowerCase();
+                            if (! tem_garagem (descricao)) {
+                                // manda = false;
                             }
                         }
                     }
@@ -73,8 +52,11 @@ function onload_func () {
                         descrdivs = linha.getElementsByTagName("a");
                         for (c = 0; c < descrdivs.length; c++) {
                             olink = descrdivs[c];
-                            if (olink.innerHTML == "Ver no site da imobiliária") {
-                                if (window.getComputedStyle(olink, null).getPropertyValue("color") == "rgb(51, 0, 153)") {
+                            if (olink.title == "Ver detalhes do imóvel") {
+                                acor = window.getComputedStyle(olink, null).getPropertyValue("color");
+                                acor = acor.toLowerCase();
+                                // Droga... Isso não funciona mais! http://hacks.mozilla.org/2010/03/privacy-related-changes-coming-to-css-vistited/
+                                if (acor == "rgb(51, 0, 153)") {
                                     manda = false;
                                 }
                                 break;
